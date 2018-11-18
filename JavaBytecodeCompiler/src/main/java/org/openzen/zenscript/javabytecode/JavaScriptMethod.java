6f2b5931dd306dee23f725820df9c6386c75ceb9
Stan Hebben <development@stanhebben.net>
2018-11-18 21:29:55 +0100

    Added missing class.

---
+++ JavaBytecodeCompiler/src/main/java/org/openzen/zenscript/javabytecode/JavaScriptMethod.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openzen.zenscript.javabytecode;
+
+import org.openzen.zenscript.codemodel.FunctionParameter;
+import org.openzen.zenscript.javashared.JavaMethod;
+import org.openzen.zenscript.javashared.JavaParameterInfo;
+
+/**
+ *
+ * @author Hoofdgebruiker
+ */
+public class JavaScriptMethod {
+	public final JavaMethod method;
+	public final FunctionParameter[] parameters;
+	public final JavaParameterInfo[] parametersInfo;
+	
+	public JavaScriptMethod(JavaMethod method, FunctionParameter[] parameters, JavaParameterInfo[] parametersInfo) {
+		this.method = method;
+		this.parameters = parameters;
+		this.parametersInfo = parametersInfo;
+	}
+}
