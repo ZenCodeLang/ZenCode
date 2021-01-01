59cb37cae57d5c3c9658cdf930cb3d45fe670e6c
kindlich <kindlich@gmx.net>
2021-01-01 18:10:42 +0100

    Added Tests for array<->list conversion

--- ScriptingExample/src/test/java/org/openzen/zenscript/scriptingexample/tests/actual_test/arrays/ArrayOperators.java
+++ ScriptingExample/src/test/java/org/openzen/zenscript/scriptingexample/tests/actual_test/arrays/ArrayOperators.java
@@ -26,4 +26,39 @@ public class ArrayOperators extends ZenCodeTest {
 		logger.assertPrintOutputSize(1);
 		logger.assertPrintOutput(0, "false");
 	}
+
+	@Test
+	public void canCastToList() {
+		ScriptBuilder.create()
+				.add("var array = ['a', 'b', 'c'] as string[];")
+				.add("var list = array as stdlib.List<string>;")
+				.add("var listImplicit as stdlib.List<string>;")
+				.add("listImplicit = array;")
+				.add("println(list.length);")
+				.add("println(listImplicit.length);")
+				.execute(this);
+
+		logger.assertPrintOutputSize(2);
+		logger.assertPrintOutput(0, "3");
+		logger.assertPrintOutput(1, "3");
+	}
+
+	@Test
+	public void canCastFromList() {
+		ScriptBuilder.create()
+				.add("var list = new stdlib.List<string>();")
+				.add("list.add('a');")
+				.add("list.add('b');")
+				.add("list.add('c');")
+				.add("var array = list as string[];")
+				.add("var arrayImplicit as string[];")
+				.add("arrayImplicit = list;")
+				.add("println(array.length);")
+				.add("println(arrayImplicit.length);")
+				.execute(this);
+
+		logger.assertPrintOutputSize(2);
+		logger.assertPrintOutput(0, "3");
+		logger.assertPrintOutput(1, "3");
+	}
 }
