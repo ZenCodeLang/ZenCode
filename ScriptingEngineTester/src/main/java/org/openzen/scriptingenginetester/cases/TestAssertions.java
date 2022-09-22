416d2cde4d9157e7814a91c898d81d29c3d61095
kindlich <kindlich@gmx.net>
2022-09-23 01:08:40 +0200

    Compare errors and output using assertLinesMatch

--- ScriptingEngineTester/src/main/java/org/openzen/scriptingenginetester/cases/TestAssertions.java
+++ ScriptingEngineTester/src/main/java/org/openzen/scriptingenginetester/cases/TestAssertions.java
@@ -1,16 +1,15 @@
 package org.openzen.scriptingenginetester.cases;
 
 import org.junit.jupiter.api.Assertions;
-import org.openzen.scriptingenginetester.TestException;
 import org.openzen.scriptingenginetester.TestOutput;
 import org.openzen.zencode.shared.CompileExceptionCode;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class TestAssertions {
 	private final List<String> expectedOutput;
@@ -47,7 +46,26 @@ public class TestAssertions {
 	}
 
 	public void validate(TestOutput output) {
-		Assertions.assertArrayEquals(expectedOutput.toArray(), output.output.toArray());
+		// Output
+		Assertions.assertLinesMatch(expectedOutput, output.output, "Test may only return what was added in '#output:' Preprocessors");
 
+		// Errors
+		final String format = "%s:%s %s"; // file:line error-name
+		final Stream<String> expectedErrors = this.expectedErrors.stream()
+				.map(expectedError -> String.format(
+						format,
+						expectedError.filename,
+						expectedError.line,
+						expectedError.error.name())
+				);
+
+		final Stream<String> actualErrors = output.exceptions.stream()
+				.map(actualError -> String.format(
+						format,
+						actualError.getPosition().getFilename(),
+						actualError.getPosition().getFromLine(),
+						actualError.error.code.name())
+				);
+		Assertions.assertLinesMatch(expectedErrors, actualErrors, "Test may only throw errors what were specified as '#error:' Preprocessors");
 	}
 }
