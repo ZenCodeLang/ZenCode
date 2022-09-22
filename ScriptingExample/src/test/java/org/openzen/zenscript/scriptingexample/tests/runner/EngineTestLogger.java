716108e06aefe0c1561d8bab66621669e97498dd
kindlich <kindlich@gmx.net>
2022-09-23 01:07:12 +0200

    WIP: Have logger write to sout for now for easier debugging

--- ScriptingExample/src/test/java/org/openzen/zenscript/scriptingexample/tests/runner/EngineTestLogger.java
+++ ScriptingExample/src/test/java/org/openzen/zenscript/scriptingexample/tests/runner/EngineTestLogger.java
@@ -1,10 +1,8 @@
 package org.openzen.zenscript.scriptingexample.tests.runner;
 
 import org.openzen.scriptingenginetester.TestOutput;
-import org.openzen.zencode.java.logger.ScriptingEngineLogger;
 import org.openzen.zencode.java.logger.ScriptingEngineStreamLogger;
 import org.openzen.zencode.shared.CompileException;
-import org.openzen.zencode.shared.SourceFile;
 import org.openzen.zenscript.scriptingexample.tests.PrintLogger;
 import org.openzen.zenscript.validator.ValidationLogEntry;
 
@@ -17,16 +15,19 @@ public class EngineTestLogger extends ScriptingEngineStreamLogger implements Pri
 
 	@Override
 	public void logCompileException(CompileException exception) {
+		super.logCompileException(exception);
 		exceptions.add(exception);
 	}
 
 	@Override
 	public void logValidationError(ValidationLogEntry errorEntry) {
+		super.logValidationError(errorEntry);
 		// TODO
 	}
 
 	@Override
 	public void logValidationWarning(ValidationLogEntry warningEntry) {
+		super.logValidationWarning(warningEntry);
 		// TODO
 	}
 
