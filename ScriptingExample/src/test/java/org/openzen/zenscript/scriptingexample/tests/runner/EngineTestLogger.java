5fc6f92de5b4b42db5da756b521994608cf9a98c
Stan Hebben <stan@stanhebben.net>
2024-03-15 20:30:53 +0100

    Fix parsing errors not being included in the test

--- ScriptingExample/src/test/java/org/openzen/zenscript/scriptingexample/tests/runner/EngineTestLogger.java
+++ ScriptingExample/src/test/java/org/openzen/zenscript/scriptingexample/tests/runner/EngineTestLogger.java
@@ -3,6 +3,8 @@ package org.openzen.zenscript.scriptingexample.tests.runner;
 import org.openzen.scriptingenginetester.TestOutput;
 import org.openzen.zencode.java.logger.ScriptingEngineStreamLogger;
 import org.openzen.zencode.shared.CompileException;
+import org.openzen.zenscript.codemodel.compilation.CompileErrors;
+import org.openzen.zenscript.lexer.ParseException;
 import org.openzen.zenscript.scriptingexample.tests.PrintLogger;
 import org.openzen.zenscript.validator.ValidationLogEntry;
 
@@ -23,6 +25,12 @@ public class EngineTestLogger extends ScriptingEngineStreamLogger implements Pri
 	public void logValidationError(ValidationLogEntry errorEntry) {
 		super.logValidationError(errorEntry);
 		exceptions.add(new CompileException(errorEntry.position, errorEntry.error));
+	}
+
+	@Override
+	public void logParseException(ParseException exception) {
+		super.logParseException(exception);
+		exceptions.add(new CompileException(exception.position, CompileErrors.parseError(exception.message)));
 	}
 
 	@Override
